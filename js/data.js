de0ab90201e8212df3487706a4b0ff24aba28346
allan.bakesiga <allan.bakesiga@duke.edu>
2026-08-12 00:05:44 -0500

    Add Endocrine thyroid question session recording

--- js/data.js
+++ js/data.js
@@ -562,7 +562,8 @@ window.BLOCKS = [
       },
       { id: "endo-thyroid",            label: "Thyroid",                    days: [],
         recordings: [
-          { title: "Endocrine: Thyroid gland (embryology, anatomy, physiology, pathology and pharmacology)", date: "2026-08-10", url: "https://us06web.zoom.us/rec/share/_NpAf7Q7mYVV7of-Y7D-rCxO5c_IsVAeJqnHVFZZOoFs2WmaIUn3AxB98fP32FxX.1QQxxknoVsNVS0aF" }
+          { title: "Endocrine: Thyroid gland (embryology, anatomy, physiology, pathology and pharmacology)", date: "2026-08-10", url: "https://us06web.zoom.us/rec/share/_NpAf7Q7mYVV7of-Y7D-rCxO5c_IsVAeJqnHVFZZOoFs2WmaIUn3AxB98fP32FxX.1QQxxknoVsNVS0aF" },
+          { title: "Endocrine: Thyroid gland, question session", date: "2026-08-11", url: "https://us06web.zoom.us/rec/share/5h9j2TMSOlc10cIyW9yGpc53CoSJ1bVOC_W1m0oYmTX5OtZv7aUUQz8vNduliFtn.3raq1ib8Hy70SLq8" }
         ]
       }
     ]
